b821b7e0c648a533e3502fcc52026bc00101ddce
Arya Yudha <sir.arya07@gmail.com>
2017-05-31 11:11:57 +0700

    denis code

--- app/src/main/java/winona/indira/ilo/MainActivity.java
+++ app/src/main/java/winona/indira/ilo/MainActivity.java
@@ -1,10 +1,7 @@
 package winona.indira.ilo;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -13,11 +10,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
-import winona.indira.ilo.fragment.BerandaFragment;
 import winona.indira.ilo.fragment.GmapFragment;
-import winona.indira.ilo.fragment.NavCategoryFragment;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -29,14 +23,14 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -73,9 +67,7 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+
 
         return super.onOptionsItemSelected(item);
     }
@@ -84,33 +76,33 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
 
-        Fragment fragment = null;
-        android.support.v4.app.FragmentManager sFm = getSupportFragmentManager();
+//        Fragment fragment = null;
+//        android.support.v4.app.FragmentManager sFm = getSupportFragmentManager();
         int id = item.getItemId();
-
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         switch (id) {
             case R.id.nav_home:
-                fragment = new BerandaFragment();
+            //    fragment = new BerandaFragment();
                 break;
             case R.id.nav_Category:
-                fragment = new NavCategoryFragment();
+              //  fragment = new NavCategoryFragment();
                 break;
             case R.id.nav_maps:
-                fragment = new GmapFragment();
-//                Intent intent = new Intent(this, GmapFragment.class);
-//                startActivity(intent);
-
+                //fragment = new GmapFragment();
+                GmapFragment gmapFragment = new GmapFragment();
+                transaction.addToBackStack(GmapFragment.class.getName());
+                transaction.replace(R.id.fragment_contain, gmapFragment).commit();
                 break;
             case R.id.nav_aboutApp:
                 break;
             case R.id.nav_aboutUs:
         }
 
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-
-        transaction.replace(R.id.fragment_contain, fragment);
-        transaction.addToBackStack(null);
-        transaction.commit();
+//        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+//
+//        transaction.replace(R.id.fragment_contain, fragment);
+//        transaction.addToBackStack(null);
+//        transaction.commit();
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
--- app/src/main/java/winona/indira/ilo/fragment/GmapFragment.java
+++ app/src/main/java/winona/indira/ilo/fragment/GmapFragment.java
@@ -7,50 +7,46 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import winona.indira.ilo.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class GmapFragment extends Fragment{
+public class GmapFragment extends Fragment implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private MapView mapView;
 
-//    public GmapFragment() {
-//        // Required empty public constructor
-//    }
+    public GmapFragment() {
+        // Required empty public constructor
+    }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_gmap, container, false);
-
-       // Intent intent = new Intent(getActivity(), MapsActivity.class);
-       // getActivity().startActivity(intent);
-
-
-       // return null;
-
+        View view = inflater.inflate(R.layout.fragment_gmap, container, false);
+        return view;
     }
 
-     /* @Override
-    public void onViewCreated (View view, Bundle savedInstanceState) {
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState){
         super.onViewCreated(view, savedInstanceState);
 
-
-        // FragmentManager fragmentManager = getChildFragmentManager();
-        // MapFragment fragment = (MapFragment)getFragmentManager().findFragmentById(R.id. map2);
-
-
-        SupportMapFragment supportMapFragment;
-        supportMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map2);
-        supportMapFragment.getMapAsync(this);
-
+        mapView = (MapView) view.findViewById(R.id.map);
+        mapView.onCreate(savedInstanceState);
+        mapView.onResume();
+        mapView.getMapAsync(this);
     }
+
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
@@ -60,50 +56,11 @@ public class GmapFragment extends Fragment{
         mMap.addMarker(new MarkerOptions().position(pas).title("Gentong Pasuruan"));
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(pas, 14));
 
-    }*/
-
-     @Override
-     public void onActivityCreated(Bundle savedInstanceState){
-         super.onActivityCreated(savedInstanceState);
-         setUpMapIfNeeded();
-     }
-
+    }
     @Override
-        public void onResume() {
+    public void onResume() {
         super.onResume();
-        setUpMapIfNeeded();
+        getActivity().setTitle("Peta Laundry");
     }
-
-    private void setUpMapIfNeeded() {
-//        if (mMap == null){
-//            //mMap = ((SupportMapFragment)getActivity().getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
-//            mMap = ((SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map)).getMap();
-            if (mMap !=null){
-                setUpMap();
-            }
-        }
-   // }
-
-    private void setUpMap() {
-      mMap.addMarker(new MarkerOptions().position(new LatLng(-7.9573461,112.6139054)).title("Marker"));
-//        LatLng pas = new LatLng(-7.64280732732681,112.892977148294);
-//        mMap.addMarker(new MarkerOptions().position(pas).title("Gentong Pasuruan"));
-//        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(pas, 14));
-    }
-
-//    @Override
-//    public void onViewCreated (View view, Bundle savedInstanceState) {
-//        super.onViewCreated(view, savedInstanceState);
-//
-//
-//        // FragmentManager fragmentManager = getChildFragmentManager();
-//        // MapFragment fragment = (MapFragment)getFragmentManager().findFragmentById(R.id. map2);
-//
-//
-////        SupportMapFragment supportMapFragment;
-////        supportMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.mapContainer);
-////        supportMapFragment.getMapAsync(this);
-//
-//    }
 }
 
